4558a10b4a8320cb2eb445f62dcd162fcc3c28b4
PlatonRavikovych <p.ravikovych@ortlyc.kyiv.ua>
2026-05-05 15:06:55 +0300

    qweq

--- unit_02/unit_02.js
+++ unit_02/unit_02.js
@@ -1,34 +1,44 @@
 // Task 1.
-
+let a = 7;
+let b = 9;
+console.log(a * b);
 // Task 2.
-
-
+let c = 7;
+let d = 9;
+document.querySelector('.out-2').textContent = c / d;
 // Task 3.
-
-
+let e = 3;
+let f = 5;
+document.querySelector('.out-3').textContent = e + f;
 // Task 4.
-
+let e1 = '3';
+let f1 = 5;
+document.querySelector('.out-4').textContent = e1 + f1;
 // Task 5.
-
-
+let e2 = 3;
+let f2 = 0;
+document.querySelector('.out-5').textContent = e2 / f2;
 // Task 6.
-
-
+let e3 = 3;
+let f3 = 'Hello';
+document.querySelector('.out-6').textContent = e3 + f3;
 // Task 7.
-
-
+let e4 = 3;
+let f4 = 'Hello';
+document.querySelector('.out-7').textContent = e4 * f4;
 // Task 8.
-
-function t8() {    
-    
-}
-
+document.querySelector('.b-8').onclick = function() {
+    let data = document.querySelector('.i-8').value;
+    document.querySelector('.out-8').textContent = data;
+};
 document.querySelector('.b-8').onclick = t8;
 
 // Task 9.
-
-function t9() {
-    
+document.querySelector('.b-9').onclick = function t9() {
+    let input = document.querySelector('.i-9');
+    let data = input.value;
+    document.querySelector('.out-9').textContent = data;
+    input.value = ''; 
 }
 
 document.querySelector('.b-9').onclick = t9;
@@ -36,33 +46,34 @@ document.querySelector('.b-9').onclick = t9;
 // Task 10.
 
 function t10() {
-    
+    let data = document.querySelector('.i-10').value;
+    document.querySelector('.out-10').textContent = data * 20;
 }
 
 document.querySelector('.b-10').onclick = t10;
 
 // Task 11.
-
 function t11() {
-    
+    let input = document.querySelector('.i-11').value;
+    document.querySelector('.out-11').innerHTML = input + 55;
 }
-
 
 document.querySelector('.b-11').onclick = t11;
 
 // Task 12.
-
 function t12() {
-    
+    let name = document.querySelector('.i-12-1').value;
+    let surname = document.querySelector('.i-12-2').value;
+    document.querySelector('.out-12').innerHTML = 'Hello ' + name + ' ' + surname;
 }
-
 
 document.querySelector('.b-12').onclick = t12;
 
 // Task 13.
-
 function t13() {
-    
+    let a = document.querySelector('.i-13-1').value;
+    let b = document.querySelector('.i-13-2').value;
+    document.querySelector('.out-13').innerHTML = Number(a) + Number(b);
 }
 
 document.querySelector('.b-13').onclick = t13;
@@ -70,9 +81,8 @@ document.querySelector('.b-13').onclick = t13;
 // Task 14.
 
 function t14() {
-    
+    document.querySelector('.i-14').value = 'Go';
 }
-
 
 document.querySelector('.b-14').onclick = t14;
 

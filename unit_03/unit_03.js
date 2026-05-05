7aff3621b9a1afb144b14fbd8818685b092e210c
PlatonRavikovych <p.ravikovych@ortlyc.kyiv.ua>
2026-05-05 16:07:22 +0300

    v3

--- unit_03/unit_03.js
+++ unit_03/unit_03.js
@@ -1,9 +1,9 @@
 // Task 1
 
-function f1(){
-   let inputValue = document.querySelector('.i-1').value;
-   let result = (inputValue == 4);
-   document.querySelector('.out-1').textContent = result;
+function f1() {
+    let inputValue = +document.querySelector('.i-1').value;
+    let result = (inputValue == 4);
+    document.querySelector('.out-1').textContent = result;
 }
 
 document.querySelector('.b-1').onclick = f1;
@@ -12,8 +12,8 @@ document.querySelector('.b-1').onclick = f1;
 // Task 2
 
 
-function f2(){
-  
+function f2() {
+
 }
 
 document.querySelector('.b-2').onclick = f2;
@@ -21,11 +21,11 @@ document.querySelector('.b-2').onclick = f2;
 
 // Task 3
 
-function f3(){
-   let num1 = document.querySelector('.i-31').value;
-   let num2 = document.querySelector('.i-32').value;
-   let result = (num1 > num2)? num1 : num2;
-   document.querySelector('.out-3').textContent = result;
+function f3() {
+    let num1 = document.querySelector('.i-31').value;
+    let num2 = document.querySelector('.i-32').value;
+    let result = (num1 > num2) ? num1 : num2;
+    document.querySelector('.out-3').textContent = result;
 }
 
 document.querySelector('.b-3').onclick = f3;
@@ -35,18 +35,18 @@ document.querySelector('.b-3').onclick = f3;
 
 
 function f4() {
-  let birthYear = +document.querySelector('.i-4').value;
-  let currentYear = new Date().getFullYear();
-  let result = (currentYear - birthYear >= 18)? 1 : 0;
-  document.querySelector('.out-4').textContent = result;
+    let birthYear = +document.querySelector('.i-4').value;
+    let currentYear = new Date().getFullYear();
+    let result = (currentYear - birthYear >= 18) ? 1 : 0;
+    document.querySelector('.out-4').textContent = result;
 }
 
 document.querySelector('.b-4').onclick = f4;
 
 // Task 5. 
 
-function f5(){
-   
+function f5() {
+
 }
 
 
@@ -55,8 +55,8 @@ document.querySelector('.b-5').onclick = f5;
 
 // Task 6.
 
-function f6(){
-   
+function f6() {
+
 
 }
 
@@ -64,8 +64,8 @@ document.querySelector('.b-6').onclick = f6;
 
 // Task 7.
 
-function f7(){
-   
+function f7() {
+
 }
 
 document.querySelector('.b-7').onclick = f7;
@@ -74,15 +74,15 @@ document.querySelector('.b-7').onclick = f7;
 
 
 function f8() {
-  
+
 }
 
 document.querySelector('.b-8').onclick = f8;
 
 // Task 9
 
-function f9(){
-   
+function f9() {
+
 
 }
 
@@ -90,8 +90,8 @@ document.querySelector('.b-9').onclick = f9;
 
 // Task 10
 
-function f10(){
-   
+function f10() {
+
 
 }
 
@@ -99,17 +99,17 @@ document.querySelector('.b-10').onclick = f10;
 
 // Task 11
 
-function f11(){
-   
+function f11() {
+
 
 }
 
 document.querySelector('.s-110').onchange = f11;
 
 
- 
- 
- 
+
+
+
 
 // Task 12
 
@@ -128,7 +128,7 @@ document.querySelector('.s-110').onchange = f11;
 
 
 function f13() {
-   
+
 }
 
 document.querySelector('.b-13').onclick = f13;
@@ -139,7 +139,7 @@ document.querySelector('.b-13').onclick = f13;
 
 
 function f14() {
-   
+
 }
 
 document.querySelector('.b-14').onclick = f14;
@@ -148,21 +148,22 @@ document.querySelector('.b-14').onclick = f14;
 
 
 function f15() {
-  let num1 = document.querySelector('.i-151').value;
-  let num2 = document.querySelector('.i-152').value;
-  let = document.querySelector('.i-153').value;
-  let result = 0;
-  switch (oper){
-    case '&&':
-        result = (num1 ==='1' && num2 === '1')? 1 : 0;
-        break;
-    case '||':
-        result = (num1 ==='1' || num2 === '1')? 1 : 0;
-        break;    
-    default:
-        result = 'unknown'
-  }
-  document.querySelector('.out-15').textContent = result;
+    let num1 = document.querySelector('.s-151').value;
+    let num2 = document.querySelector('.s-152').value;
+    let oper = document.querySelector('.s-153').value;
+    let result = 0;
+
+    switch (oper) {
+        case '&&':
+            result = (num1 === '1' && num2 === '1') ? 1 : 0;
+            break;
+        case '||':
+            result = (num1 === '1' || num2 === '1') ? 1 : 0;
+            break;
+        default:
+            result = 'unknown'
+    }
+    document.querySelector('.out-15').textContent = result;
 }
 
 document.querySelector('.b-15').onclick = f15;
